a728394349cdc3104818333a22f0a3c9752b0a4d
jooooonis <xowns3213@naver.com>
2022-11-17 23:59:33 +0900

    chore : message styles

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,7 +7,7 @@ socket.addEventListener('open', () => {
 });
 
 socket.addEventListener('message', (message) => {
-  console.log(message.data);
+  console.log('✉️: ', message.data);
 });
 
 socket.addEventListener('close', () => {
