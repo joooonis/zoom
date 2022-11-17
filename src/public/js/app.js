1b349da084301eabad539f9bf920d5ad9bee3391
jooooonis <xowns3213@naver.com>
2022-11-18 00:19:14 +0900

    feat : send json data on socket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,40 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+
+function makeMessage(type, payload) {
+  const message = { type, payload };
+  return JSON.stringify(message);
+}
 
 socket.addEventListener('open', () => {
   console.log('connection established!! ✨');
 });
 
 socket.addEventListener('message', (message) => {
-  console.log('✉️: ', message.data);
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
   console.log('close connection ❌');
 });
 
-function handleSubmit(event) {
+function handleNickSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
   input.value = '';
 }
 
-messageForm.addEventListener('submit', handleSubmit);
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleMessageSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
